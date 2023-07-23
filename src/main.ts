96787d238206f49fdb85f818a9d3c9e0defd2bd4
Arttu Pennanen <arttu@pennanen.org>
2023-07-23 23:31:26 +0300

    chore: prevent default for actionable events

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { Action, State } from './model'
 
 let queuedAction: Action | null = null
 
-const queueAction = (e: KeyboardEvent) => (queuedAction = KEY_TO_ACTION[e.key])
+const queueAction = (e: KeyboardEvent) => {
+  if (e.metaKey) return
+  const action = KEY_TO_ACTION[e.key]
+  if (!action) return
+  e.preventDefault()
+  queuedAction = action
+}
 
 const gameLoop = (state: State) => {
   drawToDom(state)
@@ -14,7 +20,8 @@ const gameLoop = (state: State) => {
   requestAnimationFrame(() => gameLoop(nextState(state, queuedAction)))
 }
 
-const startGameLoop = () => {
+const startGameLoop = (e: KeyboardEvent) => {
+  e.preventDefault()
   window.removeEventListener('keydown', startGameLoop)
   window.addEventListener('keydown', queueAction)
   gameLoop(createInitialState())
