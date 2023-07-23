ad67f53f0bccf2fe6c928953f90a416d43386488
Arttu Pennanen <arttu@pennanen.org>
2023-07-23 23:27:43 +0300

    feat: display next shape

--- src/dom.ts
+++ src/dom.ts
@@ -1,12 +1,14 @@
+import { TETRONIMO_MATRICES } from './constants'
+import { viewBoard, viewSequence, viewTetronimo } from './logic.lens'
 import { mergeTetronimoToBoard } from './logic.lib'
-import { State } from './model'
+import { EMPTY_CELL, State } from './model'
 
-const boardElement = document.getElementById('board')!
+const gameElement = document.getElementById('game')!
 const drawTo =
   (target: HTMLElement) =>
   (state: State): void => {
-    const board = mergeTetronimoToBoard(state[0], state[1])
-    let html = ''
+    const board = mergeTetronimoToBoard(viewBoard(state), viewTetronimo(state))
+    let html = '<div id="board">'
     for (const row of board) {
       html += '<div class="row">'
       for (const cell of row) {
@@ -14,7 +16,22 @@ const drawTo =
       }
       html += '</div>'
     }
+    html += '</div>'
+
+    const nextTetronimo = viewSequence(state)[0]
+    const nextMatrix = TETRONIMO_MATRICES[0][nextTetronimo]
+
+    html += '<div id="stats"><div id="next">'
+    for (const row of nextMatrix) {
+      html += '<div class="row">'
+      for (const cell of row) {
+        html += `<div class="cell ${cell ? nextTetronimo : EMPTY_CELL}"></div>`
+      }
+      html += '</div>'
+    }
+    html += '</div></div>'
+
     target.innerHTML = html
   }
 
-export const drawToDom = drawTo(boardElement)
+export const drawToDom = drawTo(gameElement)
